459ccfad2a7567b4650ee89cfe98761b2274d3d7
shivammahajancse <shivammahajanmarch@gmail.com>
2022-03-23 15:04:38 +0530

    initialized

---
+++ app/api/book_store/v1/books.rb
@@ -0,0 +1,16 @@
+module BookStore
+  module V1
+    class Books < Grape::API
+      version 'v1', using: :path
+      format :json
+      prefix :api
+      resource :books do
+        desc 'Return list of books'
+        get do
+          books = Book.all
+          present books
+        end
+      end
+    end
+  end
+end
